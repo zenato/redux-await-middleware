6f1f0e777c48a1004e20b813543a15e0f7336968
Yeongjin Lee <yeongjinnn@gmail.com>
2016-02-03 14:52:07 +0900

    Append error handling.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ export default function awaitMiddleware({ dispatch }) {
 
     return isPromise(action.payload)
       ? action.payload.then(
-        result => dispatch({ ...action, payload: result })
+        result => dispatch({ ...action, payload: result }),
+        error => dispatch({ ...action, payload: error, error: true })
       )
       : next(action);
   };
